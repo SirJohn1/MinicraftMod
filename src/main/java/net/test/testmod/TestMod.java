f80046a5abd65957cc252ea2a17a14069d6d63db
SirJihn1 <evgesha180308@gmail.com>
2025-11-20 20:09:36 +0700

    first

---
+++ src/main/java/net/test/testmod/TestMod.java
@@ -0,0 +1,53 @@
+package net.test.testmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.test.testmod.item.ModItems;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TestMod.MOD_ID)
+public final class TestMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "assets";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TestMod(FMLJavaModLoadingContext context) {
+        var modBusGroup = context.getModBusGroup();
+        FMLCommonSetupEvent.getBus(modBusGroup).addListener(this::commonSetup);
+
+        ModItems.register(modBusGroup);
+
+        BuildCreativeModeTabContentsEvent.BUS.addListener(TestMod::addCreative);
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+
+    private static void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.STEEL);
+        }
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
---
+++ src/main/java/net/test/testmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.test.testmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.test.testmod.TestMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> STEEL = ITEMS.register("steel",
+            () -> new  Item(new Item.Properties().setId(ITEMS.key("steel"))));
+
+
+
+    public  static void register(BusGroup busGroup)
+    {
+
+        ITEMS.register(busGroup);
+    }
+
+}
